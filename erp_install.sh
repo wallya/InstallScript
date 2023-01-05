04c8cc0c49cf2190ac6197832826e303a8a175f1
Yonas Asgedom <yonas.asgedom21@gmail.com>
2023-01-05 08:55:56 +0300

    Update erp_install.sh

--- erp_install.sh
+++ erp_install.sh
@@ -28,11 +28,11 @@ OE_VERSION="15.0"
 # Set this to True if you want to install the ERP enterprise version!
 IS_ENTERPRISE="False"
 # Set this to True if you want to install Nginx!
-INSTALL_NGINX="False"
+INSTALL_NGINX="True"
 # Set the superadmin password - if GENERATE_RANDOM_PASSWORD is set to "True" we will automatically generate a random password, otherwise we use this one
-OE_SUPERADMIN="admin"
+OE_SUPERADMIN="Yon@3254"
 # Set to "True" to generate a random password, "False" to use the variable in OE_SUPERADMIN
-GENERATE_RANDOM_PASSWORD="True"
+GENERATE_RANDOM_PASSWORD="False"
 OE_CONFIG="${OE_USER}-server"
 # Set the website name
 WEBSITE_NAME="_"
